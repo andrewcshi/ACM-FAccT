52fda52a3b2640b33ee57b658c0652d5923463af
andrewcshi <andrewpianoluv@gmail.com>
2023-12-13 17:31:11 -0800

    :wq

--- savehtml.py
+++ savehtml.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'https://facctconference.org/2020/acceptedpapers'
+url = 'https://facctconference.org/2019/acceptedpapers'
 response = requests.get(url)
-with open('facctpapers20.html', 'w') as file:
+with open('facctpapers19.html', 'w') as file:
     file.write(response.text)
